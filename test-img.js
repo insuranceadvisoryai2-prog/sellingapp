3cccefc3c6fab4c89e4ee642f0364f130d3eadb4
insuranceadvisoryai2-prog <insuranceadvisoryai2@gmail.com>
2026-06-09 17:55:32 +0530

    Delete test-img.js

--- test-img.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-
-https.get('https://images.meesho.com/images/products/461881412/zlelt.jpg', (res) => {
-  console.log('Status code for original:', res.statusCode);
-}).on('error', (e) => console.error(e));
-
-https.get('https://images.meesho.com/images/products/461881412/zlelt_512.jpg', (res) => {
-  console.log('Status code for _512:', res.statusCode);
-}).on('error', (e) => console.error(e));
